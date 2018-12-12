b194cd71611703a841698cd72a05e52c98f9c630
olivergate <oliver.kg2@gmail.com>
2018-12-12 16:58:39 +0000

    stylings added to film sections

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
+
+const styleFilms = {
+    backgroundColor: "brown",
+    display: "inline-block",
+    width: "400px",
+    margin: "20px",
+    padding: "10px",
+    borderRadius: "10px"
+
+}
+
+const filmTitle = {
+    margin: "0 auto",
+    width: "100px"
+}
+
+const imageStyle = {
+    backgroundSize: "cover",
+    height: "300px"
+}
+
+const button = {
+    width: "100%",
+    height: "50px"
+}
+
+
+
 export default ({title,imgUrl,date}) => (
-    <div>
-        <h3>{title}</h3>
-        <img src={imgUrl} alt="Img"/>
+    <div style={styleFilms}>
+        <h3 style={filmTitle}>{title}</h3>
+        <img style={imageStyle} src={imgUrl} alt="Img"/>
         <p>Date: {date}</p>
+        <button style={button} id={"button"}> You like this? You can't have this.</button>
     </div>
 )
