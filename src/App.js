266daf9da3ed9d202c1be494c4aafc72506a55ef
Max Taylor <max.mclaren.taylor@outlook.com>
2018-12-12 11:13:29 +0000

    setting up the app again

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DisplayFilmInfo from "./DisplayFilmInfo";
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import DisplayFilmInfo from "./DisplayFilmInfo";
+//
+// class App extends Component {
+//
+//     constructor(props)
+//     {
+//         super(props)
+//         this.state = [{name: "goldeneye", image: "hello", date: "4466"}, [{name: "octopussy", image: "hello", date: "567"}]
+//         this.dataFetch()
+//             this.filmBond()
+//     }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//
+//       </div>
+//       ( function () {
+//           var array = this.state;
+//
+//           array.map( a => <
+//
+//           )
+//           }
+//
+//       )
+//     );
+//   }
+//     dataFetch = () => {
+//         fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
+//             .then((data) => {
+//                 return data.json()
+//             })
+//             .then( (data) => {
+//                 this.setState[{name: "goldeneye", image: "", date: ""}]
+//                 this.setState({doubleDeckers:parseFloat(this.state.price/0.6).toFixed(0)})
+//
+//             })
+//     }
+//
+//
+//
+// }
+//
+// export default App;
 
-class App extends Component {
+import React from 'react'
+import { render } from 'react-dom'
+// import App from './components/App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+// import reducers from './reducers'
+import thunk from 'redux-thunk';
 
-    constructor(props)
-    {
-        super(props)
-        this.state = [{name: "goldeneye", image: "hello", date: "4466"}, [{name: "octopussy", image: "hello", date: "567"}]
-        this.dataFetch()
-            this.filmBond()
-    }
+$.get('/notes/').done((res) => {
+    const store = createStore(
+        // reducers,
+        () => {},
+        {
+            // notes: res
+        },
+        applyMiddleware(thunk)
+    )
 
-  render() {
-    return (
-      <div className="App">
+    render(
+        <Provider store={store}>
+            <div>hello world </div>
+        </Provider>,
+        document.getElementById('app')
+    )
+})
 
-
-
-
-          <DisplayFilmInfo/>
-
-      </div>
-    );
-  }
-    dataFetch = () => {
-        fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
-            .then((data) => {
-                return data.json()
-            })
-            .then( (data) => {
-                this.setState[{name: "goldeneye", image: "", date: ""}]
-                this.setState({doubleDeckers:parseFloat(this.state.price/0.6).toFixed(0)})
-
-            })
-    }
-
-
-    filmBond = () => {
-        var films = this.state
-        
-
-
-        return response;
-    }
-
-}
-
-export default App;
