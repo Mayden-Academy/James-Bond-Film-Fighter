28e34a635385c5ec380bf963aa4368a08028fc4e
olivergate <oliver.kg2@gmail.com>
2018-12-13 16:42:29 +0000

    added a score init

--- server/getBondFilms.js
+++ server/getBondFilms.js
@@ -1,6 +1,10 @@
 //this method will output all 22 bond films
-module.exports.getAllFilms = function (db) {
+module.exports.getATwoRandomFilms = function (db) {
     // let collection = db.collection('films')
     return db.aggregate([{ $sample: { size: 2 } }]).toArray()
 }
 
+module.exports.getAllFilms = function (db) {
+    // let collection = db.collection('films')
+    return db.aggregate([{ $sample: { size: 2 } }]).toArray()
+}
--- server/server.js
+++ server/server.js
@@ -19,18 +19,34 @@ const client = new mongoClient(url, { useNewUrlParser: true })
 
 app.get('/films/random', function(req, res) {
 
-    res.setHeader("Content-Type", "application/json")
+        res.setHeader("Content-Type", "application/json")
 
-    client.connect((err, client) => {
-        const db = client.db(dbName)
-        const collection = db.collection('films')
-        response = BondFilms.getAllFilms(collection)
+        client.connect((err, client) => {
+            const db = client.db(dbName)
+            const collection = db.collection('films')
+            response = BondFilms.getATwoRandomFilms(collection)
 
-        response.then(function (data) {
-            res.send(data)
+            response.then(function (data) {
+                res.send(data)
+            })
         })
-    })
-}
+    }
+)
+
+app.get('/films', function(req, res) {
+
+        res.setHeader("Content-Type", "application/json")
+
+        client.connect((err, client) => {
+            const db = client.db(dbName)
+            const collection = db.collection('films')
+            response = BondFilms.getAllFilms(collection)
+
+            response.then(function (data) {
+                res.send(data)
+            })
+        })
+    }
 )
 
 app.post('/films', jsonParser, function (req, res) {
