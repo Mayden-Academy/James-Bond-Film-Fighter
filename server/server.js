541e720b4797e5771adde8f72017d2d63712abfb
olivergate <oliver.kg2@gmail.com>
2018-12-13 16:00:27 +0000

    all the backend stuff complete for post init.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const mongoClient  = require('mongodb').MongoClient
 const assert = require('assert')
 const BondFilms = require('./getBondFilms')
 const bodyParser = require('body-parser')
+const scorer = require('./scorer')
 
+const jsonParser = bodyParser.json()
 const app = express()
 const port = 3001
 
@@ -31,12 +33,22 @@ app.get('/films/random', function(req, res) {
 }
 )
 
-app.post('/films/score'),
+app.post('/films', jsonParser, function (req, res) {
+    console.log(req.body)
+    res.send("You are working ha")
+
+    client.connect((err, client) => {
+        const db = client.db(dbName)
+        scorer.updateScores(db, req.body)
+    })
+})
+
+
 
 app.use(express.static(path.join(__dirname + '/../public')))
 
 
-app.get('/', function(req, res) {
+app.get('/',  function(req, res) {
     res.sendFile(path.join(__dirname + '/../public/index.html'))
 })
 
