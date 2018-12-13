20095f4edd638ba57633507dd6a557c1d2ef5169
Max Taylor <max.mclaren.taylor@outlook.com>
2018-12-13 17:16:45 +0000

    Update display scores

--- src/components/Film.js
+++ src/components/Film.js
@@ -35,7 +35,9 @@ export default ({data, films, refreshSubmit}) => (
         <h3 style={filmTitle}>{data.name}</h3>
         <img style={imageStyle} src={data.image} alt="Img"/>
         <p>Date: {data.release_date}</p>
-        <p>Current Score: {(data.winCount/data.playedCount).toFixed(3)*1000} </p>
+        <p>Win ratio: {((data.winCount/data.playedCount)*100).toFixed(0)}% </p>
+        <p>Wins: {data.winCount}</p>
+        <p>Losses: {data.playedCount - data.winCount}</p>
         <button onClick={() => refreshSubmit(data, films)} id={"button"}> You like this? You can't have this.</button>
     </div>
 )
