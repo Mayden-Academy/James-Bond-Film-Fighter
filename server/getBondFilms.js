92319b6035f53d54487439947ffaf74a8471e9a7
Abdi Abdiaziz <a.abdiaziz@hotmail.co.uk>
2018-12-13 17:19:52 +0000

    Mate what is going ongit add -pgit add -p

--- server/getBondFilms.js
+++ server/getBondFilms.js
@@ -1,4 +1,4 @@
 //this method will output all 22 bond films
 module.exports.getATwoRandomFilms = function (db) {
-    // let collection = db.collection('films')
+    return db.aggregate([{ $sample: { size: 2 } }]).toArray()
 }
